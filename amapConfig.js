aef577f0039956677479c86cf35a9a0c4fad9df0
BobLiu <bobliu0518@gmail.com>
2024-03-07 10:19:34 +0800

    Change AMap Key verify method

--- amapConfig.js
+++ amapConfig.js
@@ -1,10 +1,5 @@
-window.AMapKey = "58f554805b8f627e52635f3b2ee172e4";
-/*
- * window._AMapSecurityConfig = {
- *     serviceHost:'https://your.proxy.domain.here/_AMapService',  
- * }
- */
+window.AMapKey = '2a9f127b7aa146ffbb68e393ae1982d3';
 
 window._AMapSecurityConfig = {
-    securityJsCode:'8a3f1c3c0875539b7bc3073fc8ba170b',
-}
+    serviceHost: 'https://amap.bobliu.tech/_AMapService',
+};
