1b7e89b5b351b5ac7e8693f515de085cef1f7b3c
CSXianShi <123544568+CSXianShi@users.noreply.github.com>
2024-08-19 19:53:26 +0800

    Update amapConfig.js

--- amapConfig.js
+++ amapConfig.js
@@ -1,4 +1,4 @@
-window.AMapKey = '2a9f127b7aa146ffbb68e393ae1982d3';
+window.AMapKey = 'f7042c4b83e3f8a8029d04249562c2dd';
 
 window._AMapSecurityConfig = {
     serviceHost: 'https://amap.bobliu.tech/_AMapService',
